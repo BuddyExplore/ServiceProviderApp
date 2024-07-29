227cc3335cc5c97a7c36bc1f08da564e1bcda337
IsukaV <isukapremathilake@gmail.com>
2024-07-30 02:32:12 +0530

    Add shop added

--- components/Shop/FilterItems.jsx
+++ components/Shop/FilterItems.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router'
 
-const FilterItems = () => {
+const FilterItems = ({backPressed}) => {
     const router = useRouter();
+
+    const handleBackPressed = () => {
+        backPressed(1);
+    }
   return (
     <View >
         <View style={{
@@ -12,8 +16,7 @@ const FilterItems = () => {
                 flexDirection:'row',
                 gap:10,
                 alignItems:'center',
-                backgroundColor: 'white',
-                borderRadius: 20,
+                borderRadius: 30,
                 width: 150,
                 height: 50,
                 marginTop:12,
@@ -21,6 +24,26 @@ const FilterItems = () => {
                 marginLeft: 10,
                 padding: 10
             }}>
+            
+        <TouchableOpacity style={styles.touchable} onPress={handleBackPressed}>
+            <View style={{
+                display:'flex',
+                flexDirection:'row',
+                gap:10,
+                alignItems:'center',
+                justifyContent: 'center',
+                backgroundColor: '#E8E8E8',
+                borderRadius: 20,
+                width: 50,
+                height: 50,
+                marginTop:12,
+                marginBottom: 10,
+                marginLeft: 10,
+                padding: 10
+            }}>
+                <Ionicons name="arrow-back-outline" size={24} color={'black'} />
+            </View>
+        </TouchableOpacity>
         <TouchableOpacity style={styles.touchable}>
             <View style={{
                 display:'flex',
